e0b6871ae6178e870bc996c7854f4db0d32e39b8
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-11-07 18:26:48 +0530

    add

--- src/components/ToastComp.js
+++ src/components/ToastComp.js
@@ -1,17 +1,17 @@
-import { Slide, toast, Zoom } from "react-toastify";
+import { MdDonutLarge } from "react-icons/md";
+import { Bounce, Slide, toast, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function Toast({ msg, type }) {
     return toast(msg, {
         position: "top-right",
         autoClose: 4000,
-        fontSize:20,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
-        transition: Zoom,
-        progress: undefined,
+        transition: Bounce,
+        progress: 1,
         type: type,
         theme: "light",
         
